271c7b1ef77c472f17487f3894f1aa441b070c16
Umme Muqaddisa <muqaddisaumme@gmail.com>
2025-06-14 11:50:22 +0600

    .

---
+++ Candies & Two Sisters.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+ int main()
+ {
+     int a;
+     cin>>a;
+
+     while(a--)
+     {
+          int candies;
+          cin>>candies;
+
+          int ways=(candies-1)/2;
+          cout<<ways<<endl;
+     }
+ }
