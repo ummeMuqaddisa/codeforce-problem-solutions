9964d80a5530e0f946e1c47faf72279d0d6e6e7e
Umme Muqaddisa <muqaddisaumme@gmail.com>
2025-08-17 19:55:52 +0600

    .

---
+++ Again Twenty Five.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include<cmath>
+using namespace std;
+
+
+int main()
+
+{
+    long long number;
+    cin>>number;
+
+
+
+    if(number==0)
+    {
+        cout<<"01";
+    }
+    else if(number==1)
+    {
+        cout<<"05";
+    }
+    else
+    {
+        cout<<"25";
+    }
+}
+
